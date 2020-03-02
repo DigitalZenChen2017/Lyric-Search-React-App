25bbc0d43d7081cf02b52db23afb096ab428370f
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2020-03-02 11:39:54 -0500

    Added Top 10 Tracks and Lyrics Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import NavBar from './components/layout/NavBar';
 import Index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Provider } from './context';
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <NavBar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Index} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <NavBar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
